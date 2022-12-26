28c0c764116b9b3dfe136e3dd2a1f0a68b02680a
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2022-12-26 20:30:53 +0530

    stream error is coming

--- src/components/EmojiCard/index.js
+++ src/components/EmojiCard/index.js
@@ -1,12 +1,17 @@
 import './index.css'
 
 const EmojiCard = props => {
-  const {emojiDetails} = props
-  const {emojiName, emojiUrl} = emojiDetails
+  const {emojiDetails, updateScore, shuffleEmojis} = props
+  const {emojiName, emojiUrl, id} = emojiDetails
+
+  const onClickEmoji = () => {
+    updateScore(id)
+    shuffleEmojis()
+  }
 
   return (
     <li className="list-item-container">
-      <button className="emoji-btn" type="button">
+      <button className="emoji-btn" type="button" onClick={onClickEmoji}>
         <img src={emojiUrl} alt={emojiName} className="emoji-img" />
       </button>
     </li>
