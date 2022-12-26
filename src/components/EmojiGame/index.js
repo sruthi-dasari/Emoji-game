7640c6cf09fa21cbe0bc39bc9699b152e1c49bfd
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2022-12-26 20:29:09 +0530

    stream error coming

--- src/components/EmojiGame/index.js
+++ src/components/EmojiGame/index.js
@@ -1,24 +1,84 @@
 import {Component} from 'react'
 import './index.css'
 import EmojiCard from '../EmojiCard'
-import Navbar from '../NavBar'
+import NavBar from '../NavBar'
+import WinOrLoseCard from '../WinOrLoseCard'
+import {PassThrough} from 'stream'
 
 class EmojiGame extends Component {
   state = {
     score: 0,
     totalScore: 0,
+    previoslyClickedList: [],
+    gameEnd: false,
+    shuffledEmojisList: [],
+  }
+
+  shuffleEmojis = () => {
+    const shuffledEmojisList = () => {
+      const {emojisList} = this.props
+      return emojisList.sort(() => Math.random() - 0.5)
+    }
+
+    this.setState({shuffledEmojisList})
+  }
+
+  resetScore = () => {
+    this.setState({score: 0})
+  }
+
+  updateScore = clickedId => {
+    const {previoslyClickedList} = this.state
+
+    if (previoslyClickedList.length() === 0) {
+      this.setState(prevState => ({
+        previoslyClickedList: [...prevState.previoslyClickedList, clickedId],
+      }))
+    }
+
+    const isEmojiRepeated = previoslyClickedList.some(id => clickedId === id)
+
+    if (isEmojiRepeated) {
+      const {score, totalScore, gameEnd} = this.state
+
+      if (score > totalScore) {
+        this.setState({totalScore: score})
+      }
+
+      this.setState({gameEnd: true})
+    } else {
+      this.setState(prevState => ({score: prevState.score + 1}))
+      this.setState(prevState => ({
+        previoslyClickedList: [...prevState.previoslyClickedList, clickedId],
+      }))
+    }
   }
 
   render() {
     const {emojisList} = this.props
-    const {score, totalScore} = this.state
+    const {score, totalScore, gameEnd, shuffledEmojisList} = this.state
+
+    let authButton
+
+    if (score === 12) {
+      authButton = <WinOrLoseCard score={score} resetScore={this.resetScore} />
+    } else if (gameEnd === true) {
+      authButton = <WinOrLoseCard score={score} resetScore={this.resetScore} />
+    }
+
     return (
       <div className="app-container">
         <NavBar score={score} totalScore={totalScore} />
         <div className="emojigame-container">
+          {authButton}
           <ul className="emojicards-container">
-            {emojisList.map(eachEmoji => (
-              <EmojiCard emojiDetails={eachEmoji} key={eachEmoji.id} />
+            {shuffledEmojisList.map(eachEmoji => (
+              <EmojiCard
+                emojiDetails={eachEmoji}
+                key={eachEmoji.id}
+                updateScore={this.updateScore}
+                shuffleEmojis={this.shuffleEmojis}
+              />
             ))}
           </ul>
         </div>
