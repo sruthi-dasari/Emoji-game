15a81ac334d42592d3b0b5d5918067f787861146
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2022-12-26 20:31:50 +0530

    stream error is coming

--- src/components/WinOrLoseCard/index.js
+++ src/components/WinOrLoseCard/index.js
@@ -1,1 +1,57 @@
-// Write your code here.
+import './index.css'
+
+const WinOrLoseCard = props => {
+  const {score, resetScore} = props
+
+  let authButton
+
+  const onClickPlayAgain = () => {
+    resetScore()
+  }
+
+  if (score === 12) {
+    authButton = (
+      <div className="Win-card-container">
+        <img
+          className="win-lose-img"
+          src="https://assets.ccbp.in/frontend/react-js/won-game-img.png"
+          alt="win img"
+        />
+        <h1 className="win-lose-text">You Won</h1>
+        <p className="score-title-text">Best Score</p>
+        <h1 className="score-text">{score}/12</h1>
+        <button
+          className="play-again-btn"
+          type="button"
+          onClick={onClickPlayAgain}
+        >
+          Play Again
+        </button>
+      </div>
+    )
+  } else {
+    authButton = (
+      <div className="Lose-card-container">
+        <img
+          className="win-lose-img"
+          src="https://assets.ccbp.in/frontend/react-js/lose-game-img.png"
+          alt="lose img"
+        />
+        <h1 className="win-lose-text">You Lose</h1>
+        <p className="score-title-text">Score</p>
+        <h1 className="score-text">{score}/12</h1>
+        <button
+          className="play-again-btn"
+          type="button"
+          onClick={onClickPlayAgain}
+        >
+          Play Again
+        </button>
+      </div>
+    )
+  }
+
+  return <div className="WinOrLoseCard-container">{authButton}</div>
+}
+
+export default WinOrLoseCard
